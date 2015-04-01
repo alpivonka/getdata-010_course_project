b4fdeda9996738e53ef0110897079278aadc6da0
unknown <w798470@usadc-shds04.quintiles.net>
2015-04-01 16:18:02 -0400

    Update

--- Testing.R
+++ Testing.R
@@ -6,15 +6,15 @@ main2<-function(){
   xx<- read.csv("testing.csv")
   #View(xx)
   #subject_id  activity	tBodyAcc_Mean_X	tBodyAcc_Mean_Y	tBodyAcc_Mean_Z
-  #yy<-gather(xx,coltype,values,-X,-subject_id,-activity)
+  yy<-gather(xx,coltype,values,-X,-subject_id,-activity)
   #View(yy)
-  #yy<-separate(data=yy,col=coltype, into=c("type","stat","axis"))
-  #View(yy)
-  gby<-group_by(xx,activity)
-  gby %>% summarise_each(funs(mean))
-  aa<-subset(xx,select=tBodyAcc_Mean_X:fBodyGyroJerkMag_Std)
-  tidy = aggregate(aa, by=list(activity = xx$activity,subject=xx$subject), mean,na.rm=TRUE)
-  View(tidy)
+  yy<-separate(data=yy,col=coltype, into=c("type","stat","axis"), extra="merge")
+  View(yy)
+  #gby<-group_by(xx,activity)
+  #gby %>% summarise_each(funs(mean))
+  #aa<-subset(xx,select=tBodyAcc_Mean_X:fBodyGyroJerkMag_Std)
+  #tidy = aggregate(aa, by=list(activity = xx$activity,subject=xx$subject), mean,na.rm=TRUE)
+  #View(tidy)
   
   
   
